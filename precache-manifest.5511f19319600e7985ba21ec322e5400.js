442051158317fd45a2cf148c80d458a6af0f3c05
kamalyesh <kkamalyesh@gmail.com>
2021-11-13 20:36:39 +0530

    Updates

---
+++ precache-manifest.5511f19319600e7985ba21ec322e5400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e9b007f433405b71db1869fb01b105",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83f227c94990d492f900",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "5ba23d26d0852741ba2a",
+    "url": "./static/js/2.6d896fe7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6d896fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f227c94990d492f900",
+    "url": "./static/js/main.c5d1c998.chunk.js"
+  },
+  {
+    "revision": "6cc33c92b59c79351806",
+    "url": "./static/js/runtime-main.3fb888e9.js"
+  }
+]);
